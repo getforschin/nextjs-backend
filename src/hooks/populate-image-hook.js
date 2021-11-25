65f332216d8edbbe245820814b383108202f6bd3
sachin kumar <get4sachin@gmail.com>
2021-11-25 07:48:34 +0530

    client image in json reply

--- src/hooks/populate-image-hook.js
+++ src/hooks/populate-image-hook.js
@@ -4,18 +4,48 @@ const { fastJoin } = require("feathers-hooks-common");
 module.exports = function (options = {}) {
   // eslint-disable-line no-unused-vars
   return async (context) => {
-    // const { params , data , app } = context;
+     const {  app } = context;
     // console.log(data)
     // data.client = data.client.map(item => {
     //   app.service('photos-collection').get(item.photo).then(res => console.log(res))
     //   return item
     // })
 
-    const {app , data} = context
+    let data = context.result.data
+    data = data[0]
+   
     const photoCollectionService = app.service('photos-collection')
 
-    const clientArray = data.client.map(item => item.photo)
-    console.log(clientArray)
+    // console.log(data)
+
+    // data.map(item => {
+    //   item.
+    // })
+    //  const clientArray = data.client.map(item => item.photo)
+
+
+    
+
+    //  data.clientArray = await photoCollectionService.find({
+    //    query : {_id : {$in : clientArray}},
+    //    paginate : false
+    //  })
+
+    //  console.log(data.clientArray)
+
+      await Promise.all(data.client.map(async item => {
+         let _data = await photoCollectionService.get(item.photo)
+         item.image = _data
+         
+         return item
+     })).then(result => {
+       data.client = result
+     })
+     //console.log(data.client)
+
+      //console.log(clientArray)
+     //data.clientArray = clientArray
+    // console.log(clientArray)
     // fastJoin({
     //   joins : {
     //     images : () => async detail => detail.client = await photoCollectionService.find({
